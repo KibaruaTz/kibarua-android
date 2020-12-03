6d5b6c584106b9adc344fc8fd9351f4877dd7afd
joshua mshana <mama27j@gmail.com>
2020-12-03 15:21:47 +0300

    initial commit

---
+++ app/src/main/java/tz/co/kibarua/kibarua/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package tz.co.kibarua.kibarua.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
